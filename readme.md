c946efee6db60c6ab6a37b6b225b4859fd59a9b1
Sqyd <wojkap10@proton.me>
2026-01-16 21:27:20 +0100

    Init

---
+++ readme.md
@@ -0,0 +1,3 @@
+Randix - random characters terminal filler
+Copyright (C) 2026-present Sqydev/_Sqyd_/Sqyd/Wojciech
+License: gnu gpl 3.0
---
+++ src/main.c
@@ -0,0 +1,23 @@
+/*
+ * Randix - random characters terminal filler
+ * Copyright (C) 2026-present Sqydev/_Sqyd_/Sqyd/Wojciech
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Original repo: https://github.com/Sqydev/randix.git
+ * Link to author: https://github.com/Sqydev
+*/
+
+int main(int argc, char** argv) {
+	
+}
