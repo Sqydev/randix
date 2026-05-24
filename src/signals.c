c53f266a9f25b261584391ed8c82ce94f59a0799
Sqyd <138494523+Sqydev@users.noreply.github.com>
2026-05-24 23:26:22 +0000

    Update signals.c

--- src/signals.c
+++ src/signals.c
@@ -1,21 +1,36 @@
-/*
- * Randix - random characters terminal filler
- * Copyright (C) 2026-present Sqydev/_Sqyd_/Sqyd/Wojciech
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
- * Original repo: https://github.com/Sqydev/randix.git
- * Link to author: https://github.com/Sqydev
+/* 
+* Copyright (c) 2025-present Wojciech Kaptur ( _Sqyd_ / Sqydev )
+* Github: https://github.com/Sqydev
+* GPG Fingerprint: 6DC2516B0DFDA9C59661650722F7B8A777F33B56
+*
+* This software is provided "as-is", without any express or implied warranty. In no event
+* will the authors be held liable for any damages arising from the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose, including commercial
+* applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+* 1. Non-Misrepresentation: The origin of this software must not be misrepresented; 
+*    you must not claim that you wrote the original software. An acknowledgment in 
+*    product documentation is appreciated but not required.
+*
+* 2. Source-Level Copyleft: Any altered versions (forks) of this software's source code, 
+*    or files containing significant portions of this code, must be distributed under 
+*    these same license terms. Such modified source code must be made publicly available 
+*    to any recipient, even if used over a network (SaaS).
+* 
+* 3. Proprietary Integration: This software may be integrated into, linked with, or 
+*    used as a component of proprietary and closed-source products. In such cases, 
+*    the surrounding proprietary application code does not need to be disclosed, 
+*    provided that the original or modified source code of THIS software remains 
+*    available under the terms of Section 2.
+*
+* 4. Persistent Metadata: All original credits, including those in the source code headers 
+*    and binary metadata (e.g., ELF .comment section, PE StringFileInfo, or equivalent), 
+*    must not be removed. You may add your own credits to forks, provided the original 
+*    authorship remains clearly identified.
+* 
+* 5. Notice Retention: This license notice may not be removed or altered from any 
+*    source or binary distribution.
 */
 
 #include <stdlib.h>
